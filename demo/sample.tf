3bd4404f14401b69e0a747be52fb4f27435810d4
keerthi reddy <keerthireddi01@gmail.com>
2023-06-18 10:57:48 +0530

    fgnfh

--- demo/sample.tf
+++ demo/sample.tf
@@ -1,3 +1,8 @@
 resource "aws_instance" "web" {
-   ami   = "ami 
+   ami   = "ami   "
+   instance_type = "t3.micro"
+
+   tags = {
+     name = "helloworld"
+   }
 }
--- vars/vars.tf
+++ vars/vars.tf
@@ -19,3 +19,40 @@ output "sample1" {
 output "sample-ext" {
     value = "value of sample - ${var.sample}"
 }
+
+# variable data types
+#1.string
+#2.number
+#3.boolean
+
+#varibale types
+#in ansible we have 1.plain key value 2.list 3.map
+#In terraform
+#1.plain
+#2.list
+#3.map
+
+variable "plain" {
+     default = "dev training"
+}
+
+variable "courses" {
+     default = {
+        "devops"
+        "aws"
+        "python"
+     }
+}
+
+#msp
+
+variable "course details" {
+    default = {
+        devops = {
+            name = "devops"
+            timing = "11am"
+            duration = 90 mim
+        }
+        aws
+    }
+}
